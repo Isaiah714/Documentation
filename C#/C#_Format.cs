95db554305f0f06c32cc7c51f7f7054c294f7080
Isaiah714 <Ip714@csu.fullerton.edu>
2024-02-19 15:40:24 -0800

    Start up format for C#.

--- C#/C#_Format.cs
+++ C#/C#_Format.cs
@@ -1,1 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
+namespace C#_Format
+{
+    interface class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Start to write code on this line.");
+        }
+    }  
+}
