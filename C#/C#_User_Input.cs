9030d189c2dde0707064b07c844715271d97ea7a
Isaiah P <157643531+Isaiah714@users.noreply.github.com>
2024-02-19 15:48:49 -0800

    C#_User_Input.cs
    Syntax for user input.

--- C#/C#_User_Input.cs
+++ C#/C#_User_Input.cs
@@ -1,1 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           System.Console.Write("Enter you name: "); // This allows the user to input data.
+
+           string name = Console.ReadLine(); // This stores the data into a variable.
+
+           System.Console.Write("Enter you age: ");
+
+           string age = Console.ReadLine();
+
+           System.Console.WriteLine("Hello " + name + "\n");
+
+           System.Console.WriteLine("You are " + age + " years old.");
+
+
+        }
+    }
+}
 
