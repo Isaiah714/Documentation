df10b063c0835cdbfb2506c48a3f9a09495bf46b
Isaiah P <157643531+Isaiah714@users.noreply.github.com>
2024-02-19 21:29:10 -0800

    C#_Methods\Cfunction.cs
    Shows how methods work in C#.

--- C#/C#_Methods\Cfunction.cs
+++ C#/C#_Methods\Cfunction.cs
@@ -1,1 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Transactions;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Custom("peanut gun"); // Calls the method with the appropiate information.
+        }
+        static void Custom(string trinket) /* "void" is what allows you to create a method/customfunction.
+                                               You can put parameters inside the function to allow only 
+                                               certain information to be passed into the method.*/
+        {
+            System.Console.WriteLine("I got you a " + trinket);
+        }
+    }
+}
 
